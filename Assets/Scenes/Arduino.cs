1c08880c68a16839194ad112dbc4959a9304c245
XDRayLl <rayli0413@gmail.com>
2024-09-19 14:16:30 +0800

    setting

--- Assets/Scenes/Arduino.cs
+++ Assets/Scenes/Arduino.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
+using System;
 
 public class Arduino : MonoBehaviour
 {
+    public GameObject remind;
+    public GameObject final;
+    public SerialPort sp = new SerialPort("com3", 9600);
+    private bool remember = false;
+    private bool recover = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sp.Open();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
