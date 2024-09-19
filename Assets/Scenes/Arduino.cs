77fde81070b98b340f4ad3e1fc594a1c1d9aed55
XDRayLl <rayli0413@gmail.com>
2024-09-19 15:06:18 +0800

    Update Arduino.cs

--- Assets/Scenes/Arduino.cs
+++ Assets/Scenes/Arduino.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
 using System;
+using Palmmedia.ReportGenerator.Core.Common;
 
 public class Arduino : MonoBehaviour
 {
     public SerialPort sp = new SerialPort("com3", 115200);
+    public float[] num;
+    public string[]newdata;//1.¨¤«×2.x¶b3.Y¶b4.Z¶b
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +19,22 @@ public class Arduino : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        string date = sp.ReadLine();
-        
+        for (int i = 0; i < 4; i++) 
+        {
+            string date = sp.ReadLine();
+            Debug.Log(date);
+            
+            newdata[i] = null;
+            newdata[i] += date;
+            num[i] = float.Parse(newdata[i]);
+            /*if (WORD[i] != newdata[i])i
+                {
+                          WORD[i] = newdata[i];
+                      }
+                      // num[i] = float.Parse(WORD[i]);
+                      */
+        }
+        //   num[] = date.
     }
 
 }
